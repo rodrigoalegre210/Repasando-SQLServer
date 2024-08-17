30844b713ad151fc12bba0f5a9e7a293baf1e12f
Rodrigo Alegre <105827215+rodrigoprancher@users.noreply.github.com>
2024-08-17 16:33:46 -0300

    Update and rename SQL 47.sql to SQLQuery47 - Restriccion CHECK.sql

--- SQLQuery47 - Restriccion CHECK.sql
+++ SQLQuery47 - Restriccion CHECK.sql
@@ -5,7 +5,7 @@ Una empresa tiene registrados datos de sus empleados en una tabla llamada "emple
  if object_id('empleados') is not null
   drop table empleados;
 
-2- CrÈela con la siguiente estructura:
+2- Cr√©ela con la siguiente estructura:
  create table empleados (
   documento varchar(8),
   nombre varchar(30),
@@ -15,51 +15,51 @@ Una empresa tiene registrados datos de sus empleados en una tabla llamada "emple
   sueldo decimal(6,2)
  );
 
-3- Agregue una restricciÛn "check" para asegurarse que no se ingresen valores negativos para el 
+3- Agregue una restricci√≥n "check" para asegurarse que no se ingresen valores negativos para el 
 sueldo:
  alter table empleados
    add constraint CK_empleados_sueldo_positivo
    check (sueldo>0);
 
-4- Ingrese algunos registros v·lidos:
+4- Ingrese algunos registros v√°lidos:
  insert into empleados values ('22222222','Alberto Lopez','1965/10/05',1,'Sistemas',1000);
  insert into empleados values ('33333333','Beatriz Garcia','1972/08/15',2,'Administracion',3000);
- insert into empleados values ('34444444','Carlos Caseres','1980/10/05',0,'ContadurÌa',6000);
+ insert into empleados values ('34444444','Carlos Caseres','1980/10/05',0,'Contadur√≠a',6000);
 
-5- Intente agregar otra restricciÛn "check" al campo sueldo para asegurar que ninguno supere el 
+5- Intente agregar otra restricci√≥n "check" al campo sueldo para asegurar que ninguno supere el 
 valor 5000:
  alter table empleados
    add constraint CK_empleados_sueldo_maximo
    check (sueldo<=5000);
-La sentencia no se ejecuta porque hay un sueldo que no cumple la restricciÛn.
+La sentencia no se ejecuta porque hay un sueldo que no cumple la restricci√≥n.
 
-6- Elimine el registro infractor y vuelva a crear la restricciÛn:
+6- Elimine el registro infractor y vuelva a crear la restricci√≥n:
  delete from empleados where sueldo=6000;
 
  alter table empleados
    add constraint CK_empleados_sueldo_maximo
    check (sueldo<=5000); 
 
-7- Establezca una restricciÛn para controlar que la fecha de nacimiento que se ingresa no supere la 
+7- Establezca una restricci√≥n para controlar que la fecha de nacimiento que se ingresa no supere la 
 fecha actual:
  alter table empleados
    add constraint CK_fechanacimiento_actual
    check (fechanacimiento<getdate());
 
-8- Establezca una restricciÛn "check" para "seccion" que permita solamente los valores "Sistemas", 
-"Administracion" y "ContadurÌa":
+8- Establezca una restricci√≥n "check" para "seccion" que permita solamente los valores "Sistemas", 
+"Administracion" y "Contadur√≠a":
  alter table empleados
    add constraint CK_empleados_seccion_lista
    check (seccion in ('Sistemas','Administracion','Contaduria'));
 
-9- Establezca una restricciÛn "check" para "cantidadhijos" que permita solamente valores entre 0 y 
+9- Establezca una restricci√≥n "check" para "cantidadhijos" que permita solamente valores entre 0 y 
 15.
 
 10- Vea todas las restricciones de la tabla (5 filas):
  exec sp_helpconstraint empleados;
 
 11- Intente agregar un registro que vaya contra alguna de las restricciones al campo "sueldo".
-Mensaje de error porque se infringe la restricciÛn "CK_empleados_sueldo_positivo".
+Mensaje de error porque se infringe la restricci√≥n "CK_empleados_sueldo_positivo".
 
 12- Intente agregar un registro con fecha de nacimiento futura.
 Mensaje de error.
@@ -67,17 +67,17 @@ Mensaje de error.
 13- Intente modificar un registro colocando en "cantidadhijos" el valor "21".
 Mensaje de error.
 
-14- Intente modificar el valor de alg˙n registro en el campo "seccion" cambi·ndolo por uno que no 
-estÈ incluido en la lista de permitidos.
+14- Intente modificar el valor de alg√∫n registro en el campo "seccion" cambi√°ndolo por uno que no 
+est√© incluido en la lista de permitidos.
 Mensaje de error.
 
-15- Intente agregar una restricciÛn al campo secciÛn para aceptar solamente valores que comiencen 
+15- Intente agregar una restricci√≥n al campo secci√≥n para aceptar solamente valores que comiencen 
 con la letra "B":
  alter table empleados
    add constraint CK_seccion_letrainicial
    check (seccion like '%B');
-Note que NO se puede establecer esta restricciÛn porque va en contra de la establecida anteriormente 
-para el mismo campo, si lo permitiera, no podrÌamos ingresar ning˙n valor para "seccion".
+Note que NO se puede establecer esta restricci√≥n porque va en contra de la establecida anteriormente 
+para el mismo campo, si lo permitiera, no podr√≠amos ingresar ning√∫n valor para "seccion".
 
 */
 
@@ -159,7 +159,7 @@ CHECK (seccion LIKE 'B%');
 
 /*
 
-Una playa de estacionamiento almacena los datos de los vehÌculos que ingresan en la tabla llamada 
+Una playa de estacionamiento almacena los datos de los veh√≠culos que ingresan en la tabla llamada 
 "vehiculos".
 1- Elimine la tabla, si existe:
  if object_id('vehiculos') is not null
@@ -179,7 +179,7 @@ Una playa de estacionamiento almacena los datos de los vehÌculos que ingresan en
  insert into vehiculos values('CAA258','auto','2007/01/17 8:10',null);
  insert into vehiculos values('DSE367','moto','2007/01/17 8:30','2007/01/17 18:00');
 
-4- Agregue una restricciÛn "check" que especifique un patrÛn de 3 letras y 3 dÌgitos para "patente":
+4- Agregue una restricci√≥n "check" que especifique un patr√≥n de 3 letras y 3 d√≠gitos para "patente":
  alter table vehiculos
    add constraint CK_vehiculos_patente_patron
    check (patente like '[A-Z][A-Z][A-Z][0-9][0-9][0-9]');
@@ -188,44 +188,44 @@ Una playa de estacionamiento almacena los datos de los vehÌculos que ingresan en
  insert into vehiculos values('AB1234','auto',getdate(),null);
 No lo permite.
 
-6- Agregue una restricciÛn "check" que especifique que el campo "tipo" acepte solamente los valores 
+6- Agregue una restricci√≥n "check" que especifique que el campo "tipo" acepte solamente los valores 
 "auto" y "moto":
  alter table vehiculos
    add constraint CK_vehiculos_tipo_valores
    check (tipo in ('auto','moto'));
 
 7- Intente modificar el valor del campo "tipo" ingresando un valor inexistente en la lista de 
-valores permitidos por la restricciÛn establecida a dicho campo:
+valores permitidos por la restricci√≥n establecida a dicho campo:
  update vehiculos set tipo='bici' where patente='AIC124';
 No lo permite.
 
-8- Agregue una restricciÛn "default" para el campo "tipo" que almacene el valor "bici":
+8- Agregue una restricci√≥n "default" para el campo "tipo" que almacene el valor "bici":
  alter table vehiculos
    add constraint DF_vehiculos_tipo
    default 'bici'
    for tipo;
-Lo acepta. Pero, note que va en contra de la restricciÛn "check" impuesta en el punto 6.
+Lo acepta. Pero, note que va en contra de la restricci√≥n "check" impuesta en el punto 6.
 
 9- Intente ingresar un registro sin valor para "tipo":
   insert into vehiculos values('SDF134',default,null,null);
-No lo permite porque va contra la restricciÛn "check" del campo.
+No lo permite porque va contra la restricci√≥n "check" del campo.
 
-10- Agregue una restricciÛn "check" para asegurarse que la fecha de entrada a la playa no sea 
+10- Agregue una restricci√≥n "check" para asegurarse que la fecha de entrada a la playa no sea 
 posterior a la fecha y hora actual:
  alter table vehiculos
    add constraint CK_vehiculos_fechahoraentrada_actual
    check (fechahoraentrada<=getdate());
 
-11- Agregue otra restricciÛn "check" al campo "fechahoraentrada" que establezca que sus valores no 
+11- Agregue otra restricci√≥n "check" al campo "fechahoraentrada" que establezca que sus valores no 
 sean posteriores a "fechahorasalida":
  alter table vehiculos
    add constraint CK_vehiculos_fechahoraentradasalida
    check (fechahoraentrada<=fechahorasalida);
 
-12- Intente ingresar un valor que no cumpla con la primera restricciÛn establecida en el campo 
+12- Intente ingresar un valor que no cumpla con la primera restricci√≥n establecida en el campo 
 "fechahoraentrada":
  insert into vehiculos values('ABC123','auto','2007/05/05 10:10',null);
-La inserciÛn no se realiza.
+La inserci√≥n no se realiza.
 
 13- Intente modificar un registro para que la salida sea anterior a la entrada:
  update vehiculos set fechahorasalida='2007/01/17 7:30'
@@ -236,7 +236,7 @@ Mensaje de error.
  exec sp_helpconstraint vehiculos;
 aparecen 5 filas, 4 correspondientes a restricciones "check" y 1 a "default".
 
-15- Establezca una restricciÛn "default" para el campo "fechahoraentrada" para que almacene la fecha 
+15- Establezca una restricci√≥n "default" para el campo "fechahoraentrada" para que almacene la fecha 
 actual del sistema:
  alter table vehiculos
    add constraint DF_vehiculos_fechahoraentrada
