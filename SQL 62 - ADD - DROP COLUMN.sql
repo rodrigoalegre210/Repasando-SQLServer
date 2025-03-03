858b2151a05ad1e3317a02f0e4e4fb01bfd0d54e
rodrigoalegre210 <rodrigoalegre210@gmail.com>
2025-03-03 19:33:32 -0300

    Se actualiza el repositorio

---
+++ SQL 62 - ADD - DROP COLUMN.sql
@@ -0,0 +1,98 @@
+/*
+
+Trabaje con una tabla llamada "empleados".
+1- Elimine la tabla, si existe, créela y cargue un registro:
+ if object_id('empleados') is not null
+  drop table empleados;
+
+ create table empleados(
+  apellido varchar(20),
+  nombre varchar(20),
+  domicilio varchar(30),
+  fechaingreso datetime
+ );
+ insert into empleados(apellido,nombre) values ('Rodriguez','Pablo');
+
+2- Agregue el campo "sueldo", de tipo decimal(5,2).
+
+3- Verifique que la estructura de la tabla ha cambiado.
+
+4- Agregue un campo "codigo", de tipo int con el atributo "identity".
+
+5- Intente agregar un campo "documento" no nulo.
+No es posible, porque SQL Server no permite agregar campos "not null" a menos que se especifique un 
+valor por defecto.
+
+6- Agregue el campo del punto anterior especificando un valor por defecto:
+ alter table empleados
+  add documento char(8) not null default '00000000';
+
+7- Verifique que la estructura de la tabla ha cambiado.
+
+8- Elimine el campo "sueldo".
+
+9- Verifique la eliminación:
+ exec sp_columns empleados;
+
+10- Intente eliminar el campo "documento".
+no lo permite.
+
+11- Elimine los campos "codigo" y "fechaingreso" en una sola sentencia.
+
+12- Verifique la eliminación de los campos:
+ exec sp_columns empleados;
+
+*/
+
+-- 1
+IF OBJECT_ID('empleados') IS NOT NULL
+	DROP TABLE empleados;
+
+CREATE TABLE empleados(
+	apellido		VARCHAR(30),
+	nombre			VARCHAR(30),
+	domicilio		VARCHAR(30),
+	fechaingreso	DATETIME
+);
+
+insert into empleados(apellido,nombre) values ('Rodriguez','Pablo');
+
+-- 2
+ALTER TABLE empleados
+ADD sueldo DECIMAL(5,2);
+
+-- 3
+EXEC sp_columns empleados;
+
+-- 4
+ALTER TABLE empleados
+ADD codigo INTEGER IDENTITY;
+
+-- 5
+ALTER TABLE empleados
+ADD documento CHAR(8) NOT NULL;
+
+-- 6
+ALTER TABLE empleados
+ADD documento CHAR(8) NOT NULL DEFAULT '00000000';
+
+-- 7
+EXEC sp_columns empleados;
+
+-- 8
+ALTER TABLE empleados
+DROP COLUMN sueldo;
+
+-- 9
+EXEC sp_columns empleados;
+
+-- 10
+ALTER TABLE empleados
+DROP COLUMN documento;
+
+-- 11
+ALTER TABLE empleados
+DROP COLUMN codigo, fechaingreso;
+
+-- 12
+EXEC sp_columns empleados;
