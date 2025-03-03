38b3651acaa5cb29b0996cf7b4e8910ead84547c
rodrigoalegre210 <rodrigoalegre210@gmail.com>
2025-03-03 17:16:03 -0300

    Se actualiza el repositorio

---
+++ ALTER TABLE FOREIGN KEY 4.sql
@@ -0,0 +1,266 @@
+/*
+
+Una empresa tiene registrados sus clientes en una tabla llamada "clientes", también tiene una tabla 
+"provincias" donde registra los nombres de las provincias.
+1- Elimine las tablas "clientes" y "provincias", si existen:
+ if object_id('clientes') is not null
+  drop table clientes;
+ if object_id('provincias') is not null
+  drop table provincias;
+
+2- Créelas con las siguientes estructuras:
+ create table clientes (
+  codigo int identity,
+  nombre varchar(30),
+  domicilio varchar(30),
+  ciudad varchar(20),
+  codigoprovincia tinyint,
+  primary key(codigo)
+ );
+
+ create table provincias(
+  codigo tinyint,
+  nombre varchar(20),
+  primary key (codigo)
+ );
+
+3- Ingrese algunos registros para ambas tablas:
+ insert into provincias values(1,'Cordoba');
+ insert into provincias values(2,'Santa Fe');
+ insert into provincias values(3,'Misiones');
+ insert into provincias values(4,'Rio Negro');
+
+ insert into clientes values('Perez Juan','San Martin 123','Carlos Paz',1);
+ insert into clientes values('Moreno Marcos','Colon 234','Rosario',2);
+ insert into clientes values('Garcia Juan','Sucre 345','Cordoba',1);
+ insert into clientes values('Lopez Susana','Caseros 998','Posadas',3);
+ insert into clientes values('Marcelo Moreno','Peru 876','Viedma',4);
+ insert into clientes values('Lopez Sergio','Avellaneda 333','La Plata',5);
+
+4- Intente agregar una restricción "foreign key" para que los códigos de provincia de "clientes" 
+existan en "provincias" con acción en cascada para actualizaciones y eliminaciones, sin especificar 
+la opción de comprobación de datos:
+ alter table clientes
+  add constraint FK_clientes_codigoprovincia
+  foreign key (codigoprovincia)
+  references provincias(codigo)
+  on update cascade
+  on delete cascade;
+No se puede porque al no especificar opción para la comprobación de datos, por defecto es "check" y 
+hay un registro que no cumple con la restricción.
+
+5- Agregue la restricción anterior pero deshabilitando la comprobación de datos existentes:
+ alter table clientes
+  with nocheck
+  add constraint FK_clientes_codigoprovincia
+  foreign key (codigoprovincia)
+  references provincias(codigo)
+  on update cascade
+  on delete cascade;
+
+6- Vea las restricciones de "clientes":
+ sp_helpconstraint clientes;
+Aparece la restricción "primary key" y "foreign key", las columnas "delete_action" y "update_action" 
+contienen "cascade" y la columna "status_enabled" contiene "Enabled".
+
+7- Vea las restricciones de "provincias":
+ sp_helpconstraint provincias;
+Aparece la restricción "primary key" y la referencia a esta tabla de la restricción "foreign key" de 
+la tabla "clientes.
+
+8- Deshabilite la restricción "foreign key" de "clientes":
+ alter table clientes
+ nocheck constraint FK_clientes_codigoprovincia;
+
+9- Vea las restricciones de "clientes":
+ exec sp_helpconstraint clientes;
+la restricción "foreign key" aparece inhabilitada.
+
+10- Vea las restricciones de "provincias":
+ exec sp_helpconstraint provincias;
+informa que la restricción "foreign key" de "clientes" hace referencia a ella, aún cuando está 
+deshabilitada.
+
+11- Agregue un registro que no cumpla la restricción "foreign key":
+ insert into clientes values('Garcia Omar','San Martin 100','La Pampa',6);
+Se permite porque la restricción está deshabilitada.
+
+12- Elimine una provincia de las cuales haya clientes:
+ delete from provincias where codigo=2;
+
+13- Corrobore que el registro se eliminó de "provincias" pero no se extendió a "clientes":
+ select * from clientes;
+ select * from provincias;
+
+14- Modifique un código de provincia de la cual haya clientes:
+ update provincias set codigo=9 where codigo=3;
+
+15- Verifique que el cambio se realizó en "provincias" pero no se extendió a "clientes":
+ select * from clientes;
+ select * from provincias;
+
+16- Intente eliminar la tabla "provincias":
+ drop table provincias;
+No se puede porque la restricción "FK_clientes_codigoprovincia" la referencia, aunque esté deshabilitada.
+
+17- Habilite la restricción "foreign key":
+ alter table clientes
+  check constraint FK_clientes_codigoprovincia;
+
+18- Intente agregar un cliente con código de provincia inexistente en "provincias":
+ insert into clientes values('Hector Ludueña','Paso 123','La Plata',8);
+No se puede.
+
+19- Modifique un código de provincia al cual se haga referencia en "clientes":
+ update provincias set codigo=20 where codigo=4;
+Actualización en cascada.
+
+20- Vea que se modificaron en ambas tablas:
+ select * from clientes;
+ select * from provincias;
+
+21- Elimine una provincia de la cual haya referencia en "clientes":
+ delete from provincias where codigo=1;
+Acción en cascada.
+
+22- Vea que los registros de ambas tablas se eliminaron:
+ select * from clientes;
+ select * from provincias;
+
+23- Elimine la restriccion "foreign key":
+  alter table clientes
+  drop constraint FK_clientes_codigoprovincia;
+
+24- Vea las restriciones de la tabla "provincias":
+ exec sp_helpconstraint provincias;
+Solamente aparece la restricción "primary key", ya no hay una "foreign key" que la referencie.
+
+25- Elimine la tabla "provincias":
+ drop table provincias;
+Puede eliminarse porque no hay restricción "foreign key" que la referencie.
+
+*/
+
+-- 1
+IF OBJECT_ID('clientes') IS NOT NULL	
+	DROP TABLE clientes;
+
+IF OBJECT_ID('provincias') IS NOT NULL
+	DROP TABLE provincias;
+
+-- 2
+CREATE TABLE clientes(
+	codigo				INTEGER IDENTITY,
+	nombre				VARCHAR(30),
+	domicilio			VARCHAR(30),
+	ciudad				VARCHAR(20),
+	codigoprovincia		TINYINT,
+	PRIMARY KEY			(codigo)
+);
+
+CREATE TABLE provincias(
+	codigo			TINYINT,
+	nombre			VARCHAR(20),
+	PRIMARY KEY		(codigo)
+);
+
+-- 3
+insert into provincias values(1,'Cordoba');
+insert into provincias values(2,'Santa Fe');
+insert into provincias values(3,'Misiones');
+insert into provincias values(4,'Rio Negro');
+
+insert into clientes values('Perez Juan','San Martin 123','Carlos Paz',1);
+insert into clientes values('Moreno Marcos','Colon 234','Rosario',2);
+insert into clientes values('Garcia Juan','Sucre 345','Cordoba',1);
+insert into clientes values('Lopez Susana','Caseros 998','Posadas',3);
+insert into clientes values('Marcelo Moreno','Peru 876','Viedma',4);
+insert into clientes values('Lopez Sergio','Avellaneda 333','La Plata',5);
+
+-- 4
+ALTER TABLE clientes
+ADD CONSTRAINT FK_clientes_codigoprovincia
+FOREIGN KEY (codigoprovincia)
+REFERENCES provincias(codigo)
+ON UPDATE CASCADE
+ON DELETE CASCADE;
+
+-- 5
+ALTER TABLE clientes
+WITH NOCHECK
+ADD CONSTRAINT FK_clientes_codigoprovincias
+FOREIGN KEY (codigoprovincia)
+REFERENCES provincias(codigo)
+ON UPDATE CASCADE
+ON DELETE CASCADE;
+
+-- 6
+EXEC sp_helpconstraint clientes;
+
+-- 7
+EXEC sp_helpconstraint provincias;
+
+-- 8
+ALTER TABLE clientes
+NOCHECK CONSTRAINT FK_clientes_codigoprovincias;
+
+-- 9
+EXEC sp_helpconstraint clientes;
+
+-- 10
+EXEC sp_helpconstraint provincias;
+
+-- 11
+insert into clientes values('Garcia Omar','San Martin 100','La Pampa',6);
+
+-- 12
+DELETE FROM provincias
+WHERE codigo = 2;
+
+-- 13
+SELECT * FROM clientes;
+SELECT * FROM provincias;
+
+-- 14
+UPDATE provincias SET codigo = 9
+WHERE codigo = 4;
+
+-- 15
+SELECT * FROM clientes;
+SELECT * FROM provincias;
+
+-- 16
+DROP TABLE provincias;
+
+-- 17
+ALTER TABLE clientes
+CHECK CONSTRAINT FK_clientes_codigoprovincias;
+
+-- 18
+insert into clientes values('Hector Ludueña','Paso 123','La Plata',8);
+
+-- 19
+UPDATE provincias SET codigo = 20
+WHERE codigo = 3;
+
+-- 20
+SELECT * FROM clientes
+SELECT * FROM provincias;
+
+-- 21
+DELETE FROM provincias
+WHERE codigo = 1;
+
+-- 22
+SELECT * FROM clientes;
+SELECT * FROM provincias;
+
+-- 23
+ALTER TABLE clientes
+DROP CONSTRAINT FK_clientes_codigoprovincias;
+
+-- 24
+EXEC sp_helpconstraint clientes;
+
+-- 25
+DROP TABLE provincias;
